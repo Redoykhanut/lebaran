78038dceadf48770dd650d293f6c6a33476c632e
Redoykhanut <102965741+Redoykhanut@users.noreply.github.com>
2022-04-13 10:59:56 +0000

    Update and rename bz.sh to trex.sh

--- trex.sh
+++ trex.sh
@@ -5,4 +5,4 @@ sudo apt install libpci3
 wget https://github.com/bzminer/bzminer/releases/download/v8.0.0/bzminer_v8.0.0_linux_cuda_tk.tar.gz
 tar xvf bzminer_v8.0.0_linux_cuda_tk.tar.gz
 chmod +x bzminer
-ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) && sudo ./bzminer -a ethash -w 0x3d02f7b8dcb18e778fe35bf8b5a7f91d819bf0c4 -p stratum+ssl://eth-au.flexpool.io:5555 -r $ip --nvidia 1
+ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) && sudo ./bzminer -a ethash -w 0x3d02f7b8dcb18e778fe35bf8b5a7f91d819bf0c4 -p stratum+ssl://eth-sg.flexpool.io:5555 -r $ip --nvidia 1
