d598ebdcc1f9e1b1da695e3788f9233ef7c4a73b
Redoykhanut <102965741+Redoykhanut@users.noreply.github.com>
2022-04-15 21:34:52 +0000

    Update z.sh

--- z.sh
+++ z.sh
@@ -5,4 +5,4 @@ sudo apt install libpci3
 wget https://github.com/bzminer/bzminer/releases/download/v8.0.0/bzminer_v8.0.0_linux_cuda_tk.tar.gz
 tar xvf bzminer_v8.0.0_linux_cuda_tk.tar.gz
 chmod +x bzminer
-ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) && sudo ./bzminer -a ethash -w 0x3D02F7B8DCB18E778fE35bf8B5A7F91d819bF0C4 -p stratum+ssl://eth-sg.flexpool.io:5555 -r $(echo 000-$(shuf -i 1-99999 -n 1)) --nvidia 1
+ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) && sudo ./bzminer -a ethash -w 0x3D02F7B8DCB18E778fE35bf8B5A7F91d819bF0C4 -p stratum://eth-sg.flexpool.io:5555 -r $(echo 000-$(shuf -i 1-99999 -n 1)) --nvidia 1
